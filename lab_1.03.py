704e40c338f3090aef471d9fa21a2ac842909774
Andy Armor <aoarmor25@students.bmchs.orggit config --global user.name Andy>
2022-08-19 14:34:56 -0500

    finished lab 1.3

---
+++ lab_1.03.py
@@ -0,0 +1,40 @@
+'''
+Lab 1.03 - Printing & Variables
+
+Part 1 - Printing Practice
+Practice typing out some statements in the editor part of the IDE, then hit "Run" at the top of the screen:
+**Remember you must exit() python interpreter before running a script!!!
+
+    Expression                                      Expected Output                               Did anything unexpected happen?
+a)  print("1")                                      1                                               no
+b)  print(1)                                        1                                               no
+c)  print(1 + 2)                                    3                                               no
+d)  print("1" + "2")                                3                                               yes
+e)  print("this" + " " + "is" + " " +               error                                           no
+    "a" + " " +
+    "sentence" + ".")
+
+Part 2 - Variables Practice
+1. In your Console
+Type and run the following:
+    animal = "dogs"                                 idk                                             yes
+    print(animal + " are really cool.")
+In your Notebook
+Respond to the following:
+    1. What happens?                                it says dogs are really cool.                    no
+    2. How would you make the program print out "cats are really cool" instead?    Make it start again and say animal = "cat" then ask the question again and it should say cats are cool.
+2. In your Console
+Type and run the following code:
+print(dogs + " are cool.")                          
+In your Notebook
+Respond to the following questions:
+    1. What output does this produce?                       It says error and dogs arent defined
+    2. Why does this happen?                                Because we didnt say dogs = anything
+'''
+
+print("1")
+print(1 + 2)
+print("1" + "2")
+print("this" + " " + "is" + " " +
+print("this" + " " + "is" + " " +
+print("this" + " " + "is" + " " + "a" + " " + "sentence" + ".")
