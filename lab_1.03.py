83069bf0f9c5187627831705858e688c00d24646
Andy Armor <aoarmor25@students.bmchs.orggit config --global user.name Andy>
2022-08-19 14:41:47 -0500

    finishing 1.3

--- lab_1.03.py
+++ lab_1.03.py
@@ -10,27 +10,15 @@ a)  print("1")                                      1                           
 b)  print(1)                                        1                                               no
 c)  print(1 + 2)                                    3                                               no
 d)  print("1" + "2")                                3                                               yes
-e)  print("this" + " " + "is" + " " +               error                                           no
+e)  print("this" + " " + "is" + " " +               this is a sentence                              yes         
     "a" + " " +
     "sentence" + ".")
-
-Part 2 - Variables Practice
-1. In your Console
-Type and run the following:
-    animal = "dogs"                                 idk                                             yes
-    print(animal + " are really cool.")
-In your Notebook
-Respond to the following:
-    1. What happens?                                it says dogs are really cool.                    no
-    2. How would you make the program print out "cats are really cool" instead?    Make it start again and say animal = "cat" then ask the question again and it should say cats are cool.
-2. In your Console
-Type and run the following code:
 print(dogs + " are cool.")                          
 In your Notebook
 Respond to the following questions:
     1. What output does this produce?                       It says error and dogs arent defined
     2. Why does this happen?                                Because we didnt say dogs = anything
-'''
+
 
 print("1")
 print(1 + 2)
@@ -38,3 +26,14 @@ print("1" + "2")
 print("this" + " " + "is" + " " +
 print("this" + " " + "is" + " " +
 print("this" + " " + "is" + " " + "a" + " " + "sentence" + ".")
+
+    print("this" + " " + "is" + " " +                                                         
+    "a" + " " +
+    "sentence" + ".")
+    print("this" + " " + "is" + " " +                                                         
+    "a" + " " +
+    "sentence" + ".")
+'''
+print("this" + " " + "is" + " " +                                                         
+    "a" + " " +
+    "sentence" + ".")
